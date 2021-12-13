730d62382215c6bc8b98d8f96a2760f4993d585c
adarsh25t <adarsh25t@gmail.com>
2021-12-13 12:22:09 +0530

    start prime

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import Navbar from "../components/NavBar"
+
+const Home = ()=>{
+    return(
+        <Fragment>
+            <Navbar/>
+        </Fragment>
+    )
+}
+export default Home;
